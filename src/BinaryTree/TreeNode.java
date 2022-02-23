29bb36c9ab3e0fa62eb14ab6865c183947c664c5
unknown <asingh1248@gmail.com>
2022-02-23 22:43:15 +0530

    Added the code for Binary Tree

---
+++ src/BinaryTree/TreeNode.java
@@ -0,0 +1,12 @@
+package BinaryTree;
+
+public class Node {
+
+    Node left,right;
+    int key;
+
+    public Node(int key) {
+        this.key = key;
+        left=right=null; //imp
+    }
+}
--- src/LinkedList2/IntersectionTwolInkedListHashSet.java
+++ src/LinkedList2/IntersectionTwolInkedListHashSet.java
@@ -1,2 +1,44 @@
-package LinkedList2;public class IntersectionTwolInkedListHashSet {
+package LinkedList2;
+
+public class IntersectionTwolInkedListHashSet {
+
+    Node head;
+
+    public static void main(String[] args) {
+
+        IntersectionTwolInkedListHashSet linkedlist1  = new IntersectionTwolInkedListHashSet();
+
+        linkedlist1.head = new Node(1);
+        linkedlist1.head.next = new Node(2);
+        linkedlist1.head.next.next = new Node(8);
+        linkedlist1.head.next.next.next = new Node(7);
+        linkedlist1.head.next.next.next.next = new Node(6);
+
+        System.out.println("Linked List l1");
+        LinkedListCommonMethods.printlist(linkedlist1.head);
+
+        IntersectionTwolInkedListHashSet linkedlist2  = new IntersectionTwolInkedListHashSet();
+        linkedlist2.head = new Node(2);
+        linkedlist2.head.next = new Node(8);
+        linkedlist2.head.next.next = new Node(1);
+        linkedlist2.head.next.next.next = new Node(3);
+        linkedlist2.head.next.next.next.next = new Node(8);
+        linkedlist2.head.next.next.next.next.next = new Node(7);
+        linkedlist2.head.next.next.next.next.next.next = new Node(6);
+
+        System.out.println("Linked List l2");
+        LinkedListCommonMethods.printlist(linkedlist2.head);
+
+
+        IntersectionTwolInkedListHashSet anshead  = new IntersectionTwolInkedListHashSet();
+        anshead.head = findingIntersection(linkedlist1.head,linkedlist2.head);
+
+    }
+
+    private static Node findingIntersection(Node head, Node head1) {
+
+
+        return head;
+    }
+
 }
